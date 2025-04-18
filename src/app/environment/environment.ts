5647bc6ae5f24efbb800756674ca950463d9579c
devivaraprasad <vudidevivaraprasad143@gmail.com>
2025-04-18 20:54:54 +0530

    updated main url to production one

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -5,5 +5,5 @@ import { Injectable } from '@angular/core';
 })
 export class Environment {
   Api:string = "https://et1in74t39.execute-api.us-east-1.amazonaws.com/Prod"
-  Main:string = "http://127.0.0.1:4200"
+  Main:string = "https://lambda-frontend-olive.vercel.app"
 }
