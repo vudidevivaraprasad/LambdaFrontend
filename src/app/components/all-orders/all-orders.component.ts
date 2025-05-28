f1b40a0af9eccef42c5eb4a75ad3680bb4b3a9f3
devivaraprasad <vudidevivaraprasad143@gmail.com>
2025-05-28 18:31:46 +0530

    updated admin page to get all orders from backend

--- src/app/components/all-orders/all-orders.component.ts
+++ src/app/components/all-orders/all-orders.component.ts
@@ -9,53 +9,53 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class AllOrdersComponent implements OnInit {
 
-  // orders:Order[] = []
-  orders:Order[] = [
-    {
-        "product": {
-            "amount": 100,
-            "product_id": "d14f24fa-b537-46",
-            "quantity": 1
-        },
-        "address": {
-            "area": "",
-            "pincode": 531055,
-            "mobilenumber": 8367360185,
-            "city": "vishakapatanam",
-            "name": "devi varaprasad vudi",
-            "id": 1745240068172,
-            "state": "Andhra pradesh",
-            "village": ""
-        },
-        "order_id": "6cb1b440-c9cd-4b63-9875-0ef21570d317",
-        "Date": 1745240109450,
-        "status": "pending"
-    },
-    {
-        "product": {
-            "amount": 100,
-            "product_id": "d14f24fa-b537-461a-a36b-c4275f8db832",
-            "quantity": 1
-        },
-        "address": {
-            "area": "ramalayam",
-            "pincode": 531055,
-            "mobilenumber": 8367360185,
-            "city": "vishakapatanam",
-            "name": "devi varaprasad vudi",
-            "id": 1745244266071,
-            "state": "Andhra pradesh",
-            "village": "lakkavaram"
-        },
-        "order_id": "6d196572-a4ee-42c5-b91a-8da19f4af58c",
-        "Date": 1745244305473,
-        "status": "pending"
-    }
-]
+  orders:Order[] = []
+//   orders:Order[] = [
+//     {
+//         "product": {
+//             "amount": 100,
+//             "product_id": "d14f24fa-b537-46",
+//             "quantity": 1
+//         },
+//         "address": {
+//             "area": "",
+//             "pincode": 531055,
+//             "mobilenumber": 8367360185,
+//             "city": "vishakapatanam",
+//             "name": "devi varaprasad vudi",
+//             "id": 1745240068172,
+//             "state": "Andhra pradesh",
+//             "village": ""
+//         },
+//         "order_id": "6cb1b440-c9cd-4b63-9875-0ef21570d317",
+//         "Date": 1745240109450,
+//         "status": "pending"
+//     },
+//     {
+//         "product": {
+//             "amount": 100,
+//             "product_id": "d14f24fa-b537-461a-a36b-c4275f8db832",
+//             "quantity": 1
+//         },
+//         "address": {
+//             "area": "ramalayam",
+//             "pincode": 531055,
+//             "mobilenumber": 8367360185,
+//             "city": "vishakapatanam",
+//             "name": "devi varaprasad vudi",
+//             "id": 1745244266071,
+//             "state": "Andhra pradesh",
+//             "village": "lakkavaram"
+//         },
+//         "order_id": "6d196572-a4ee-42c5-b91a-8da19f4af58c",
+//         "Date": 1745244305473,
+//         "status": "pending"
+//     }
+// ]
   constructor(private api:ApiService){}
 
   ngOnInit(): void {
-    // this.api.AllOrders().subscribe(data => this.orders = data.result)
+    this.api.AllOrders().subscribe(data => this.orders = data.result)
   }
 
 }
